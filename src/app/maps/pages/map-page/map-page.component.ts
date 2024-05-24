ca5e5ac1e31992f1964e4f048cc4fe4d01b26af8
Derian <dmrodriguez2000@gmail.com>
2024-05-24 14:39:38 -0600

    project structure

---
+++ src/app/maps/pages/map-page/map-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maps-map-page',
+  templateUrl: './map-page.component.html',
+  styleUrl: './map-page.component.css'
+})
+export class MapPageComponent {
+
+}
